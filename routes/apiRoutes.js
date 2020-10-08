c217bb2dd24d02618dfda640514126cc82208b0e
Cynthia Garcia <i.cynthiagarcia@gmail.com>
2020-10-08 18:37:22 -0500

    fixed bugs for functionality

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,53 +1,58 @@
 // Dependencies
+// const { fstat } = require("fs");
 const fs = require("fs");
 
 // Routing
 module.exports = function(app) {
     // API GET request
     // Reading db.json and returning saved notes
-    app.get("/api/notes", function(req, res) {
-        fs.readFile(__dirname + "../db/db.json", (err, data) => { 
+    app.get("/api/notes", (req, res) => {
+        fs.readFile(__dirname + "/../db/db.json", (err, data) => { 
             if (err) throw err;
-            const storedNotes = JSON.parse(data);
+            let storedNotes = JSON.parse(data);
             return res.json(storedNotes);
         })
     });
 
     // API POST request
     // Saving notes
-    app.post("/api/notes", function(req, res) {
+    app.post("/api/notes", (req, res) => {
         const newNote = req.body;
         let savedNotes = [];
         let id = 0;
         
-        fs.readFile(__dirname + "/../db/db.json"), "utf8", (err, data) => {
+        fs.readFile(__dirname + "/../db/db.json", "utf8", (err, data) => { // took away "utf8" paramater between dirname and (err,data)
             if (err) throw err;
+
             savedNotes = JSON.parse(data);
-            for (var i = 0; i < savedNotes.length; i++) {
+
+            for (let i = 0; i < savedNotes.length; i++) {
                 if (savedNotes[i].id > id) {
                     id = savedNotes[i].id;
                 }
             }
+
             newNote.id = parseInt(id) + 1;
             savedNotes.push(newNote);
-            fs.writeFile(__dirname + "../db/db.json", JSON.stringify(savedNotes), "utf8", err => {
+
+            fs.writeFile(__dirname + "/../db/db.json", JSON.stringify(savedNotes), "utf8", err => {
                 if (err) throw err;
                 res.end();
             })
-        }
+        })
     })
 
     // API DELETE
     // Delete notes
-    app.delete("/api/notes:id", function(req, res) {
-        const noteTBD = req.params.id;
+    app.delete("/api/notes/:id", (req, res) => {
+        let noteTBD = req.params.id;
 
-        fs.readFile(__dirname + "../db/db.json", (err,data) => {
+        fs.readFile(__dirname + "/../db/db.json", "utf8", (err,data) => {
             if (err) throw err;
 
-            let restNotes = JSON.parse(data).filter(entry => {
-                return entry.id !== noteTBD;
-            })
+            let restNotes = JSON.parse(data).filter((entry) => {
+                return entry.id != noteTBD; // took away !==
+            });
 
             fs.writeFile(__dirname + "/../db/db.json", JSON.stringify(restNotes), "utf8", err => {
                 if (err) throw err;
--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 // Dependencies 
 const express = require("express");
+// const { dirname } = require("path"); // why need this?
 
 // Express configuration
 // Create "express" server
 const app = express();
 
 // Set initial port
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 4001;
 
 // Set up Express app to to handle data parsing (AKA: Middleware)
 app.use(express.urlencoded({ extended: true })); // urlencoded body from form submission
 app.use(express.json()); // json submission
-// app.use(express.static(__dirname + "/public")); // What is this used for?
+app.use(express.static(__dirname + "/public")); // What is this used for?
 
 // Router
 require("./routes/apiRoutes")(app);
